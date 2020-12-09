783598726898ffb69223b355cddf20b23c21448c
nico <ndeleon@itba.edu.ar>
2020-12-09 14:25:21 -0300

    Add filters

--- TP Final/TPF dsp imagenes/codigos/convfiltergen.py
+++ TP Final/TPF dsp imagenes/codigos/convfiltergen.py
@@ -6,12 +6,50 @@ import sys
 
 import numpy as np
 
-# DEFINE KERNEL TO USE:
+# HIGH BOOST A=0 DONE:
+# kernel = np.array([
+#     [-1, -1, -1],
+#     [-1, 8 + A, -1],
+#     [-1, -1, -1]
+#     ])
+
+# LOW PASS FILTER DONE! NOT TESTED
+
+#    [1, 2, 1],
+#    [2, 4 ,2],
+#    [1, 2, 1]
+
+
+# SOBEL DIRECTION X / EDGE DETECTION: DONE! NOT TESTED
+#    [-1, -2, -1],
+#    [0, 0, 0],
+#    [1, 2, 1]
+
+# SOBEL DIRECTION Y / EDGE DETECION DONE! NOT TESTED
+# -> SOBEL DIRECTION X .T
+
+# THRESHHOLD + BLUR
+
+# GRAYSCALE CONVERTION -> DONE! NOT TESTED
+# (0.3 * R) + (0.59 * G) + (0.11 * B) ).
+
+# GREEN / BLUE / RED FILTERS -> DONE! NOT TESTED
+# (0 * R) + (1 * G) + (0 * B) ).
+# ETC...
+
+# COLOR INVERSION -> DONE! NOT TESTED
+# (255 - R) + (255 - G) + (255 - B) )
+
+# BRIGTNESS
+# new_image[y,x,c] = np.clip(alpha*image[y,x,c] + beta, 0, 255)
+
+
 kernel = np.array([
-    [-1, -1, -1],
-    [-1, 8, -1],
-    [-1, -1, -1]
-    ])
+    [-1, -2, -1],
+    [0, 0, 0],
+    [1, 2, 1]
+     ]).T
+
 
 names = [
     ["upleft", "up", "upright"],
@@ -43,7 +81,7 @@ for i in range(len(names)):
 kernel_str = ""
 
 # FILTER WILL BE STORED IN /FILTROS/FILT.TXT
-sys.stdout = open("../filtros/filt.txt", "w")
+sys.stdout = open("../filtros/sobel_edge_detect_Y.txt", "w")
 
 for rgb_color, slice_ in [
     ["red", "11:8"],
